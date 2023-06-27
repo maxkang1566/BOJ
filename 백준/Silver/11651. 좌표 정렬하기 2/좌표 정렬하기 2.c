3c313f5b1dd8bece5c10a777e88b02f89669272f
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2023-06-27 23:51:43 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 68 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct coord {
+	int x;
+	int y;
+}coord;
+
+int compare(const void* a, const void* b) {
+	coord A = *(coord*)a;
+	coord B = *(coord*)b;
+	if (A.y > B.y)
+		return 1;
+	else if (A.y == B.y) {
+		if (A.x > B.x)
+			return 1;
+		else
+			return -1;
+	}
+	return -1;
+}
+
+int main(void)
+{
+	int n;
+	scanf("%d", &n);
+	coord* arr = (coord*)malloc(sizeof(coord)*n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &arr[i].x, &arr[i].y);
+	}
+	qsort(arr, n, sizeof(coord), compare);
+
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
+
+	free(arr);
+	return 0;
+}
