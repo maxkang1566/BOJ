8043ccaa0ad2b4c88ea76534b59b77e13ab7991d
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2023-07-08 18:44:43 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(void)
+{
+	int n, temp;
+	
+	queue<int> card;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		card.push(i);
+
+	while (1) {
+		temp = card.front();
+		card.pop();
+		if (card.empty())
+			break;
+
+		card.push(card.front());
+		card.pop();
+	}
+
+	cout << temp << endl;
+	return 0;
+}
