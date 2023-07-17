d7f4bd79abd716cca3f49a82c81479d82dd62c44
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2023-07-17 22:17:15 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n1, n2, k1, k2;
+	cin >> n1 >> k1 >> n2 >> k2;
+	cout << n1*k1 + n2*k2 << endl;
+
+	return 0;
+}
