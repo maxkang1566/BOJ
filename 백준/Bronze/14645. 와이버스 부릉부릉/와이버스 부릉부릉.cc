b55a53e9008144e8f8f06f4f636507df6377ec92
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2023-07-24 01:32:25 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n, k;
+	int a, b;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+	}
+	cout << "비와이" << endl;
+}
