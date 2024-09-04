84c66f8be982dd6e4fbbf132a721599e9868c2bb
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2024-09-04 09:51:42 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long d[100][2];
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+
+  d[1][0] = 0LL;
+  d[1][1] = 1LL;
+
+  for (int i = 2; i <= n; i++)
+  {
+    d[i][0] = d[i - 1][0] + d[i - 1][1];
+    d[i][1] = d[i - 1][0];
+  }
+
+  cout << d[n][0] + d[n][1];
+  return 0;
+}
