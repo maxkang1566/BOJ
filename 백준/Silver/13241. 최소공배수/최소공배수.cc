e45359658fa59e4782b0c994e6661c6ade0d50a9
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2024-10-12 20:57:47 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <numeric>
+using namespace std;
+
+int main(void)
+{
+  long long a, b;
+  long long result;
+  cin >> a >> b;
+  result = lcm(a, b);
+
+  cout << result;
+}
