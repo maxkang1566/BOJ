e78a7be388be1a5399d333f1cba1ac0dd4d15b01
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2024-10-16 20:10:22 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 284 ms, Memory: 3972 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[500005];
+int n, m;
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  sort(a, a + n);
+
+  cin >> m;
+  while (m--)
+  {
+    int t;
+    cin >> t;
+    cout << upper_bound(a, a + n, t) - lower_bound(a, a + n, t) << " ";
+  }
+}
