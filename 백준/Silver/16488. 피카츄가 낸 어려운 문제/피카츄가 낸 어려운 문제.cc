d292542b6f4c3d76ca59aff5becd383eeed14c22
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2024-12-01 14:57:53 +0900

    [Silver IV] Title: 피카츄가 낸 어려운 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16488. 피카츄가 낸 어려운 문제/피카츄가 낸 어려운 문제.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  long long n, k, res;
+  cin >> n >> k;
+  res = n * n * k;
+  cout << res;
+  return 0;
+}
