c2130885a758427b076a56ecc8b1c8dd7052c57d
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2025-06-05 18:57:17 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,30 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, k;
-int arr[1000005];
 
+int n, k, temp, psum[100001], ret = -10000004;
 int main(void)
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
 
-  int temp = 0, result;
   cin >> n >> k;
-  for (int i = 0; i < n; i++)
-    cin >> arr[i];
-
-  for (int i = 0; i < k; i++)
-    temp += arr[i];
-
-  result = temp;
-
-  for (int i = 1; i < n - k + 1; i++)
+  for (int i = 1; i <= n; i++)
   {
-    temp = temp - arr[i - 1] + arr[i + k - 1];
-    result = max(result, temp);
+    cin >> temp;
+    psum[i] = psum[i - 1] + temp;
   }
-
-  cout << result;
+  for (int i = k; i <= n; i++)
+  {
+    ret = max(ret, psum[i] - psum[i - k]);
+  }
+  cout << ret << "\n";
   return 0;
 }
