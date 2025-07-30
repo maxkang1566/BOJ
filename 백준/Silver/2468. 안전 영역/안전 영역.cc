8186a58528453eacf8b1205eda95e11801d9efe7
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2025-07-30 15:39:59 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -2,65 +2,50 @@
 using namespace std;
 #define X first
 #define Y second
+int board[105][105];
+bool vis[105][105]={};
+int n, mx=0;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
-int board[101][101];
-bool vis[101][101];
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int n;
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-void init_vis() {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			vis[i][j] = 0;
-}
+    cin >> n;
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            cin >> board[i][j];
 
-int main(void)
-{
-	int maxHeight = -1, maxArea = 0;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
+    for(int height=0; height<101; height++) {
+        int num = 0;
+        for(int temp = 0; temp < 105; temp++)
+            fill(vis[temp], vis[temp] + 105, 0);
 
-			if (board[i][j] > maxHeight)
-				maxHeight = board[i][j];
-		}
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j]<=height || vis[i][j]) continue;
+                num++;
+                queue<pair<int, int>> Q;
+                vis[i][j] = 1;
+                Q.push({i, j});
+                while(!Q.empty()) {
+                    pair<int, int> cur = Q.front(); Q.pop();
+                    for(int dir = 0; dir < 4; dir++) {
+                        int nx = cur.X + dx[dir];
+                        int ny = cur.Y + dy[dir];
+                        if(nx < 0 || nx > n || ny < 0 || ny > n) continue;
+                        if(vis[nx][ny] || board[nx][ny] <= height) continue;
+                        vis[nx][ny] = 1;
+                        Q.push({nx, ny});
+                    }
+                }
+                mx = max(mx, num);
+            }
+        }
+    }
 
-	for (int rain = 0; rain <= maxHeight; rain++) {
-		int num = 0;
-		init_vis();
+    cout << mx << '\n';
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (board[i][j] > rain && !vis[i][j]) {
-					queue<pair<int, int>> Q;
-					vis[i][j] = 1;
-					Q.push({ i, j });
-
-					while (!Q.empty()) {
-						auto cur = Q.front();
-						Q.pop();
-
-						for (int dir = 0; dir < 4; dir++) {
-							int nx = cur.X + dx[dir];
-							int ny = cur.Y + dy[dir];
-
-							if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-							if (!vis[nx][ny] && board[nx][ny] > rain) {
-								vis[nx][ny] = 1;
-								Q.push({ nx, ny });
-
-							}
-						}
-					}
-					num++;
-				}
-			}
-		}
-
-		if (num > maxArea)
-			maxArea = num;
-	}
-	cout << maxArea << "\n";
+    return 0;
 }
