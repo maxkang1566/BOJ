c3b7b08526d77649b21d5fcf7ae3b529a2f641a6
maxkang1566 <128452053+maxkang1566@users.noreply.github.com>
2025-12-16 16:03:20 +0900

    [Silver V] Title: 중복된 숫자, Time: 912 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.cc
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.cc
@@ -1,23 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, temp, ret;
-int arr[10000004];
+using ll = long long;
 
 int main(void)
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
+
+  ll n, x, sum = 0;
   cin >> n;
   for (int i = 0; i < n; i++)
   {
-    cin >> temp;
-    arr[temp]++;
-    if (arr[temp] > 1)
-    {
-      ret = temp;
-      break;
-    }
+    cin >> x;
+    sum += x;
   }
-  cout << ret << "\n";
-  return 0;
+
+  ll expected = (n - 1) * n / 2;
+  cout << sum - expected << "\n";
 }
